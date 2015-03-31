f6a4afe4686d29477ec6aa438713199934bb6523
Kamil Lelonek <squixy.sln@gmail.com>
2015-03-31 09:21:02 +0200

    P01 solved

---
+++ src/test/scala/P01Spec.scala
@@ -0,0 +1,19 @@
+import collection.mutable.Stack
+import org.scalatest._
+
+class P01Spec extends FlatSpec with Matchers {
+  import P01._
+
+  it should "find the last element of a given list" in {
+    val lastElement = 3
+    val list        = List(true, "string") :: lastElement :: Nil
+    last(list) should be (lastElement)
+  }
+
+  it should "throw NoSuchElementException for an empty list" in {
+    val emptyList = List()
+    a [NoSuchElementException] should be thrownBy {
+      last(emptyList)
+    }
+  }
+}
