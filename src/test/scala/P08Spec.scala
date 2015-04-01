e854045f60f0bd068dc48e1f52311d6b71b157c5
Kamil Lelonek <squixy.sln@gmail.com>
2015-04-01 10:55:53 +0200

    P08 solved

---
+++ src/test/scala/P08Spec.scala
@@ -0,0 +1,18 @@
+import org.scalatest._
+
+class P08Spec extends FlatSpec with Matchers {
+  import P08._
+
+  it should "compress a given list" in {
+    compress(List(1, 2, 2, 3, 3, 3)) should be (List(1, 2, 3))
+  }
+
+  it should "do nothing with an already unique list" in {
+    val uniqueElementsList = List(1, 2, 3)
+    compress(uniqueElementsList) should be (uniqueElementsList)
+  }
+
+  it should "do nothing with empty list" in {
+    compress(List()) should be (List())
+  }
+}
