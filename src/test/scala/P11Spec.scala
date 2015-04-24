a1729cf5c100bcd034d1c42ea773b91912e66141
Kamil Lelonek <squixy.sln@gmail.com>
2015-04-24 22:17:51 +0200

    P11 improved

--- src/test/scala/P11Spec.scala
+++ src/test/scala/P11Spec.scala
@@ -3,11 +3,11 @@ import org.scalatest._
 class P11Spec extends FlatSpec with Matchers {
   import P11._
 
-  it should "compress and count consecutive duplicates from a given lists" in {
+  it should "encode and count consecutive duplicates from a given lists but only if more than one" in {
     encodeModified(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) should be (List((4,'a), 'b, (2,'c), (2,'a), 'd, (4,'e)))
   }
 
-  it should "put each element in a single list from anunique list" in {
+  it should "not transform list when there are not consecutive duplicates" in {
     encodeModified(List(1, 2, 3)) should be (List(1, 2, 3))
   }
 
